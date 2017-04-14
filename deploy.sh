c9fb1192436c217423f2121977fcec460b3420a8
nickolay.pol@gmail.com <PoWeR26!>
2017-04-14 22:05:14 +0300

    store PID in variables and kill previous process

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+kill $(cat /tmp/my-app.pid)
 git pull
 npm install
 
 #sudo -u postgres psql < /home/srv/managers2/migrations/00000000000.sql
 
-nohup node . > /dev/null &
+nohup node . > /dev/null & echo $! >>  /tmp/my-app.pid &
+
